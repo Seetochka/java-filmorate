68f112cd4bc9e1e0a1a387462fdeb6d19ab9ef5b
heck <ramazan280699@gmail.com>
2022-06-07 14:11:42 +0300

    feet: добавить удаление пользователей по id

--- src/main/java/ru/yandex/practicum/filmorate/services/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/UserService.java
@@ -50,6 +50,15 @@ public class UserService {
     }
 
     /**
+     * Удаление пользователя
+     */
+    public void deleteUser(int id) throws ModelNotFoundException {
+        findById(id);
+
+        storage.deleteUser(id);
+    }
+
+    /**
      * Получение всех пользователей
      */
     public Collection<User> findAll() {
