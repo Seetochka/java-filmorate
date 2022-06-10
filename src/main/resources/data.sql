6a17d60ee13730e2b2bf4bfbd0d6357e3d11e812
Evgeniya <vlasova.jen@mail.ru>
2022-06-10 12:57:53 +0300

    fix: очистить данные таблицы для тестов

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-/*
+
 
 
 INSERT INTO mpa (name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
 
 INSERT INTO genre (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
 
-
+/*
 drop table FILM_DIRECTOR;
 drop table DIRECTOR;
 drop table FILM_GENRE;
@@ -16,4 +16,4 @@ drop table FRIEND;
 drop table FILM;
 drop table USER;
 
-*/
+
