7f797be6da650b33bc48de8bc3e44e9b0595add2
mmm <RobinsonKrusoe@yandex.ru>
2022-06-15 22:54:50 +0300

    fix: исправить замечания после review

--- src/main/java/ru/yandex/practicum/filmorate/controllers/EventController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/EventController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,13 @@ import ru.yandex.practicum.filmorate.services.EventService;
 import java.util.Collection;
 
 @RestController
+@RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
 
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
-    }
-
-    //Возвращает ленту событий пользователя.
+    /**
+     * Возвращает ленту событий пользователя.
+     */
     @GetMapping("/users/{id}/feed")
     public Collection<Event> getFeed(@PathVariable int id) throws Exception {
         return eventService.getAllByUser(id);
